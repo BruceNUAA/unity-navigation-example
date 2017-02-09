c1c6bfccfad56f5314409bc7d020b9afab05372c
Kyle Copeland <copelandky@gmail.com>
2017-02-09 00:22:56 -0800

    init commit

---
+++ Assets/MoveTo.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveTo : MonoBehaviour {
+
+	public Transform goal;
+
+	void Start () {
+		var agent = GetComponent<NavMeshAgent> ();	
+		agent.SetDestination (goal.position);
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Navigation In Unity Example
+===========================================
+For ICS 161 - UC Irvine
